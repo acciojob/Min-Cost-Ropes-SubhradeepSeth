b23e425f7385b7c4602f532c9e78a313065d65ae
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-11 10:44:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,17 @@ function mincost(arr)
 	});
 	let sum2=0;
 	let n=arr.length;
-	for(let i=1;i<;i++){
-		let sum1=0;
-		sum1=arr[i]+arr[i-1];
-		sum2+=sum1;
-	}
-	if(n%2!==0) sum2+=arr[n-1];
-   return sum2;
+	// for(let i=0;i<n;i+=2){
+	// 	let sum1=0;
+	// 	sum1=arr[i]+arr[i-1];
+	// 	sum2+=sum1;
+	// }
+	// if(n%2!==0) sum2+=arr[n-1];
+	
+	let res=arr.reduce(function(prev,current){
+		return prev+current;
+	})
+   return res;
 }
 
 module.exports=mincost;
