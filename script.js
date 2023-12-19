cbd4263e41bd45afb80cadfa9d3b81715c81ae14
SubhradeepSeth <139810175+SubhradeepSeth@users.noreply.github.com>
2023-12-19 10:42:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ function mincost(arr)
 	arr.sort(function(a,b){
 		return a-b;
 	});
-	let sum=0;
+	let sum=0,sum1=0;
    for(let i=1;i<arr.length;){
-	   sum+=arr[i]+arr[i-1];
-	   arr[i]=sum;
+        sum1=arr[i]+arr[i-1];
+        arr[i]=sum1;
+	   sum+=sum1;
+       sum1=0;
 	   arr.splice(0,1);
 	   arr.sort(function(a,b){
 		   return a-b;
-	   });
+	   })
    }
+   return sum;
 }
 
 module.exports=mincost;
